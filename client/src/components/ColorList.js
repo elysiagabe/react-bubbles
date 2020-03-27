a084d0d2adc9dabab342226a068f35dbcbc2dee8
Elysia Gabe <elysiagabe@gmail.com>
2020-03-27 09:19:15 -0700

    Implements edit color functionality

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +8,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  //console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -16,11 +17,29 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
+  console.log({colorToEdit});
+
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        //console.log({res});
+        const newColorArr = colors.map(color => {
+          if (color.id === colorToEdit.id) {
+            return colorToEdit
+          } else {
+            return color
+          }
+        })
+        updateColors(newColorArr);
+        setEditing(false);
+        setColorToEdit(initialColor);
+      })
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,20 +14,20 @@ const Login = props => {
     });
   };
 
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
   const handleLogin = e => {
     e.preventDefault();
     axiosWithAuth()
       .post("/api/login", credentials)
       .then(res => {
-        console.log(res);
+        //console.log(res);
         localStorage.setItem("token", res.data.payload);
         props.history.push("/bubbles");
       })
       .catch(err => console.log("Error logging in: ", err))
   };
 
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
